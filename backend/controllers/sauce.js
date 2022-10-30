f1e5eaca52fc72a3ab96c3810df106b3ea7ad4a7
c-hristophe <c-hr@live.fr>
2022-10-30 19:52:44 +0100

    révision de l'architecture back / front

--- backend/controllers/sauce.js
+++
@@ -1,9 +0,0 @@
-const Sauce = require('../models/Sauce');
-const fs = require('fs');
-
-// Lecture de toutes les sauces 
-exports.getAllSauces = (req, res, next) => {
-  Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-};
